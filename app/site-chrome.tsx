aa9425d1cf628bd9277756a7004dd7674b127d5a
Judith Nkechinyere Njoku <108449006+Judith989@users.noreply.github.com>
2026-07-21 11:40:29 -0600

    Rename World navigation to My World

--- app/site-chrome.tsx
+++ app/site-chrome.tsx
@@ -7,7 +7,7 @@ const navigation = [
   ["Home", "/"],
   ["About", "/about"],
   ["Research", "/research"],
-  ["World", "/research-world"],
+  ["My World", "/research-world"],
   ["Publications", "/publications"],
   ["News", "/news"],
   ["Gallery", "/gallery"],
@@ -42,7 +42,7 @@ export function SiteFooter() {
         <Link href="/about">About</Link>
         <Link href="/news">News</Link>
         <Link href="/gallery">Gallery</Link>
-        <Link href="/research-world">Research World</Link>
+        <Link href="/research-world">My World</Link>
         <Link href="/cv">CV</Link>
         <Link href="/contact">Contact</Link>
       </div>
